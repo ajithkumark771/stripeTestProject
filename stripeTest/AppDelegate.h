b37726b534a2be90a70a87b32c215f0622955535
Ajithkumark <ajith@cybraum.net>
2019-05-15 16:50:42 +0530

    Initial Commit

---
+++ stripeTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  stripeTest
+//
+//  Created by Cybraum on 5/15/19.
+//  Copyright © 2019 meridian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ stripeTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stripeTest
+//
+//  Created by Cybraum on 5/15/19.
+//  Copyright © 2019 meridian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
